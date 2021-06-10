4fc0d878af5ea8e242aaeae0ec1761df5d3bac8b
foushware <mr.fouad992@gmail.com>
2021-06-10 18:54:21 +0200

    🏯 apollo-server express setup

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,31 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Posts";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 const main = async () => {
 	const orm = await MikroORM.init(microConfig);
 
-	//Create post
-	const post = orm.em.create(Post, { title: "my first post" });
-	// Run the migration automatic
-	await orm.getMigrator().up();
-	//insert post to the DB
-	await orm.em.persistAndFlush(post);
-	const allPosts = await orm.em.find(Post, {});
-	console.log(allPosts);
+	//Create express server
+	const app = express();
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver],
+			validate: false,
+		}),
+	});
+	//Apply the middleware before sending the request to the server
+	apolloServer.applyMiddleware({ app });
+
+	app.get("/", (_, res) => {
+		res.send("hello");
+	});
+	app.listen(4000, () => {
+		console.log("server started on localhost:4000");
+	});
 };
 main().catch((error) => {
 	console.error(error);
---
+++ src/resolvers/hello.ts
@@ -0,0 +1,11 @@
+/** @format */
+
+import { Query, Resolver } from "type-graphql";
+
+@Resolver()
+export class HelloResolver {
+	@Query(() => String)
+	hello() {
+		return "hello World";
+	}
+}
