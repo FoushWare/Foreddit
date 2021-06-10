909cfb0d8d264af577f63d034104612a55d433b5
foushware <mr.fouad992@gmail.com>
2021-06-10 17:12:59 +0200

    🏰  mikro-ORM setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello i'm jone Doe sorry for testing git flow 🎮😂");
+/** @format */
+
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Posts";
+import microConfig from "./mikro-orm.config";
+const main = async () => {
+	const orm = await MikroORM.init(microConfig);
+
+	//Create post
+	const post = orm.em.create(Post, { title: "my first post" });
+	// Run the migration automatic
+	await orm.getMigrator().up();
+	//insert post to the DB
+	await orm.em.persistAndFlush(post);
+	const allPosts = await orm.em.find(Post, {});
+	console.log(allPosts);
+};
+main().catch((error) => {
+	console.error(error);
+});
